f77ae93116d461f89b8f82665808c2d9f84b1928
JvictorMarques <jvictorsilvamarques358@gmail.com>
2026-04-21 13:14:09 -0300

    feat(app): register movies and actors routers
    - Mount actors router at /api/v1/actors
    - Mount movies router at /api/v1/movies

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI, status
 
-from src.routers import users
+from src.routers import actors, movies, users
 
 app = FastAPI()
 
 app.include_router(prefix='/api/v1/users', tags=['users'], router=users.router)
+
+app.include_router(
+    prefix='/api/v1/movies', tags=['movies'], router=movies.router
+)
+
+app.include_router(
+    prefix='/api/v1/actors', tags=['actors'], router=actors.router
+)
 
 
 @app.get('/health', status_code=status.HTTP_200_OK)
